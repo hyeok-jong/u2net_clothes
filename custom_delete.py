fa993e4c817bc45236ebc3ef64e2288f986991fc
HyeokJong <jj770206@naver.com>
2022-05-07 14:37:09 +0900

    add 0 size

--- custom_delete.py
+++ custom_delete.py
@@ -2,8 +2,8 @@ import os
 import cv2  
 import numpy as np
 
-list_dir = os.listdir("input_images")
-os.chdir('./input_images')
+list_dir = os.listdir("test_data/test_human_images")
+os.chdir('./test_data/test_human_images')
 
 cnt = 0
 
@@ -13,6 +13,7 @@ for i in list_dir:
         cnt += 1
 print(f"deleted {cnt} images due to format")
 
+
 cnt = 0
 list_dir = os.listdir(os.getcwd())
 for i in list_dir:
@@ -21,11 +22,21 @@ for i in list_dir:
         os.remove(i)
         image_np = np.empty([2,2,3])
     image_np = image_np.shape
-    
+
     if image_np[0]*image_np[1] > 2000000:
         os.remove(i)
         cnt += 1
-print(f"deleted {cnt} images due to size")
+
+print(f"deleted {cnt} images due to size ")
 
 
+cnt = 0
+list_dir = os.listdir(os.getcwd())
+for i in list_dir:
+    size = os.path.getsize(i)
+    if size < 1:
+        os.remove(i)
+        cnt += 1
 
+
+print(f"deleted {cnt} images due to none ")
